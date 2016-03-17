b710e167f4e6f9b8ad63c7bd7f2ac8d75e6edf55
kemball <jkemball@gmail.com>
2016-03-17 14:51:36 -0500

    performnace tweaks and hmm tendons

--- karkadann/hmm.py
+++ karkadann/hmm.py
@@ -4,14 +4,20 @@ import subprocess as sp
 import threading
 from tempfile import NamedTemporaryFile as ntf
 from database import hmm_location
-from Bio import SearchIO,SeqIO
+from Bio import SearchIO,SeqIO,SeqRecord
+from database import Gene
 
 
 def call_hmmer(hmm,inputproteins):
-	hmmfile=os.path.join(hmm_location,hmm)
-	sp.call(['hmmsearch',hmmfile,inputproteins])
-	hits = SearchIO.parse(hmmfile,format=hmmer3-text)
-	for x in hits:
-		print x
+	with ntf(prefix="/dev/shm/") as inputfasta:
+		with ntf(prefix="/dev/shm/") as hmmoutput:
+			SeqIO.write(inputproteins,inputfasta,'fasta')
+			hmmfile = os.path.join(hmm_location, hmm)
+			sp.call(['hmmsearch','-o',hmmoutput.name, hmmfile, inputfasta.name])
+			hits = SearchIO.parse(hmmoutput, format="hmmer3")
+			for x in hits:
+				print x
 
-
+if __name__=="__main__":
+	input = SeqRecord.SeqRecord("QLEDKNS"*100)
+	call_hmmer("AfsA.hmm",[input])
