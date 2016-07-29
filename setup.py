bf0c6dc27612fe3a837abf7d714eb3bd4fd6358e
kemball <jkemball@gmail.com>
2016-07-29 15:59:03 -0500

    version 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="karkadann",
-	version='0.069',
+	version='1.0',
 	author="Jeremy Kemball",
 	description=("A data-processing pipeline for Gene Cluster Family creation."),
 	include_package_data=True,
